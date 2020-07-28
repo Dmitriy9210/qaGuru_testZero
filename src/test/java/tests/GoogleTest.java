4697ac0ca48206ba50041b07c7d4bbb0f9cec256
kefir <lala@mail.ru>
2020-07-28 17:12:48 +0300

    Добавлены кейсы на открытие сайта Selenide, на поиск сайта mail.ru и открытие поиска Яндекс

--- src/test/java/tests/GoogleTest.java
+++ src/test/java/tests/GoogleTest.java
@@ -3,7 +3,7 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -12,11 +12,38 @@ class GoogleTest {
     void selenideSearchTest() {
         // Открыть google
         open("https://google.com");
-
         // Ввести Selenide в поиск
         $(byName("q")).setValue("Selenide").pressEnter();
-
         // Проверить, что Selenide появился в результатах поиска
         $("html").shouldHave(text("ru.selenide.org"));
     }
+
+    @Test
+    void selenideSearchAndOpenTest() {
+        // Открыть google
+        open("https://google.com");
+        // Ввести Selenide в поиск
+        $(byName("q")).setValue("Selenide").pressEnter();
+        // Проверить, что Selenide появился в результатах поиска
+        $("html").shouldHave(text("ru.selenide.org"));
+
+        // открыть сайт selenide
+        $(byText("ru.selenide.org")).click();
+        $("html").shouldHave(text("ЧТО ТАКОЕ SELENIDE?"));
+    }
+
+    @Test
+    void selenideSearchMailRuTest() {
+        open("https://google.com");
+        $(byName("q")).setValue("mail ru").pressEnter();
+        $("html").shouldHave(text("mail.ru"));
+    }
+
+    @Test
+    void selenideSearchYandexTest() {
+        open("https://yandex.ru/");
+        $(byCssSelector(".input__control.input__input")).setValue("Selenide").pressEnter();
+        $("html").shouldHave(text("ru.selenide.org"));
+    }
+
 }
